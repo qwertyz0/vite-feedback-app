4705542cb3a34f5d84ddcc6e75766056f73218a6
YanisS <yanismac@MacBook-Pro-YanisS.local>
2023-04-06 17:15:25 +0300

    initiate vite-react project of feedback

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello vite feedback</h1>
+    </div>
+  );
+}
+
+export default App;
