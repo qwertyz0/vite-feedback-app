c9be345439381e1dce34b713c5ec25e64507ef66
YanisS <yanismac@MacBook-Pro-YanisS.local>
2023-04-12 15:17:24 +0300

    import animation for returning list (fade in and out), and comment functionality without fade animation, and add two types for id in PropTypes (bcs uuidv4 make string id)

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion, AnimatePresence } from "framer-motion"; //add fade animation
 import FeedbackItem from "./FeedbackItem";
 import PropTypes from "prop-types";
 
@@ -9,25 +10,54 @@ function FeedbackList({ feedback, handleDelete }) {
 
   return (
     <div className="feedback-list">
-      {feedback.map((item, index) => {
-        return (
-          <FeedbackItem
-            key={index}
-            id={item.id}
-            rating={item.rating}
-            text={item.text}
-            handleDelete={handleDelete}
-          />
-        );
-      })}
+      <AnimatePresence>
+        {feedback.map((item) => (
+            <motion.div //fades in and out animation
+              key={item.id}
+              initial={{ opacity: 0 }} //start like invisible 
+              animate={{ opacity: 1 }}// animate full visible
+              exit={{ opacity: 0 }}//out insisible
+            >
+              <FeedbackItem
+                key={item.id}
+                id={item.id}
+                rating={item.rating}
+                text={item.text}
+                handleDelete={handleDelete}
+              />
+            </motion.div>
+          ))}
+      </AnimatePresence>
     </div>
   );
+
+  /*version without animation */
+
+  // return (
+  // <div className="feedback-list">
+  //   {feedback.map((item, index) => {
+  //     return (
+  //       <FeedbackItem
+  //         key={index}
+  //         id={item.id}
+  //         rating={item.rating}
+  //         text={item.text}
+  //         handleDelete={handleDelete}
+  //       />
+  //     );
+  //   })}
+  // </div>
+  // );
 }
 
 FeedbackList.propTypes = {
   feedback: PropTypes.arrayOf(
     PropTypes.shape({
-      id: PropTypes.number.isRequired,
+      id: PropTypes.oneOfType([
+        // add two types for id (bcs uuidv4 make string id)
+        PropTypes.number.isRequired,
+        PropTypes.string.isRequired,
+      ]),
       text: PropTypes.string.isRequired,
       rating: PropTypes.number.isRequired,
     })
