369da2561ddd4515835292a4c5f5399b2285a2e5
YanisS <yanismac@MacBook-Pro-YanisS.local>
2023-04-12 15:24:07 +0300

    import uuidv4 and get object  data from the form and push it as new feedback in list

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
+import {v4 as uuidv4} from 'uuid'
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackData from "./data/Feedbackdata";
 import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
+
   const deleteFeedback = (id) => {
     //function to delete feedback (used props chain)
     if (window.confirm("Are you sure to delete this feedback ?")) {
@@ -13,10 +16,17 @@ function App() {
     }
   };
 
+  const addFeedback = (newFeedback) =>{
+    newFeedback.id = uuidv4()
+    setFeedback([newFeedback, ...feedback])
+    //console.log(newFeedback)
+  }
+
   return (
     <>
       <Header />
       <div className="container">
+        <FeedbackForm handleAdd={addFeedback}/>
         <FeedbackStats feedbackStat={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
