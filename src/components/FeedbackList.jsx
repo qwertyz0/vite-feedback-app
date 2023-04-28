49d1fb4870522cfc51427ec4669585e8be00c791
YanisS <yanismac@MacBook-Pro-YanisS.local>
2023-04-28 17:15:29 +0300

    add useContext hook to grab satate from Context api, and remove props of "feedback"

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { useContext } from "react";
 import { motion, AnimatePresence } from "framer-motion"; //add fade animation
 import FeedbackItem from "./FeedbackItem";
-import PropTypes from "prop-types";
+import FeedbackContext from "../context/FeedbackContext";
 
-function FeedbackList({ feedback, handleDelete }) {
+function FeedbackList({ handleDelete }) {
+  // if use context, dont need feedback props anymore
+  //we pass it from context api
+
+  const { feedback } = useContext(FeedbackContext); //pass context from global api FeedbackContext
+  //and use "feedback" state that created in context file
+
   if (!feedback || feedback.length === 0) {
     return <p>No data yet ...</p>;
   }
@@ -12,21 +19,21 @@ function FeedbackList({ feedback, handleDelete }) {
     <div className="feedback-list">
       <AnimatePresence>
         {feedback.map((item) => (
-            <motion.div //fades in and out animation
+          <motion.div //fades in and out animation
+            key={item.id}
+            initial={{ opacity: 0 }} //start like invisible
+            animate={{ opacity: 1 }} // animate full visible
+            exit={{ opacity: 0 }} //out insisible
+          >
+            <FeedbackItem
               key={item.id}
-              initial={{ opacity: 0 }} //start like invisible 
-              animate={{ opacity: 1 }}// animate full visible
-              exit={{ opacity: 0 }}//out insisible
-            >
-              <FeedbackItem
-                key={item.id}
-                id={item.id}
-                rating={item.rating}
-                text={item.text}
-                handleDelete={handleDelete}
-              />
-            </motion.div>
-          ))}
+              id={item.id}
+              rating={item.rating}
+              text={item.text}
+              handleDelete={handleDelete}
+            />
+          </motion.div>
+        ))}
       </AnimatePresence>
     </div>
   );
@@ -49,19 +56,5 @@ function FeedbackList({ feedback, handleDelete }) {
   // </div>
   // );
 }
-
-FeedbackList.propTypes = {
-  feedback: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.oneOfType([
-        // add two types for id (bcs uuidv4 make string id)
-        PropTypes.number.isRequired,
-        PropTypes.string.isRequired,
-      ]),
-      text: PropTypes.string.isRequired,
-      rating: PropTypes.number.isRequired,
-    })
-  ),
-};
 
 export default FeedbackList;
--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,28 +1,26 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { useContext } from "react";
+import FeedbackContext from "../context/FeedbackContext";
 
-function FeedbackStats({ feedbackStat }) {
+function FeedbackStats() {
+  const { feedback } = useContext(FeedbackContext); // same with all context use
+
   //calculate avarage rating showed feedbacks
   //'acc' - is free variable to plus rating of all items, using cur.rating
   // 0 for default state of 'acc'
   let avarage =
-    feedbackStat.reduce((acc, cur) => {
+    feedback.reduce((acc, cur) => {
       return acc + cur.rating;
-    }, 0) / feedbackStat.length;
+    }, 0) / feedback.length;
 
   avarage = avarage.toFixed(1).replace(/[.,]0$/, "");
   // (/[.,]0$/, '') - regular expression to replace 0 if number without . or ,
 
   return (
     <div className="feedback-stats">
-      <h4>{feedbackStat.length} Reviews</h4>
+      <h4>{feedback.length} Reviews</h4>
       <h4>Avarage rating: {isNaN(avarage) ? "0" : avarage}</h4>
     </div>
   );
 }
-
-FeedbackStats.propTypes = {
-  feedbackStat: PropTypes.array.isRequired,
-};
 
 export default FeedbackStats;
