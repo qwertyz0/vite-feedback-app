acf5afcf2adaa5dc7ddc320903c25b8d1481224d
YanisS <yanismac@MacBook-Pro-YanisS.local>
2023-05-10 16:54:02 +0300

    create fetch function to grab data throw api and start making spinner gif for loading

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 // context api for get access to all components without using props
 // for state changes and etc.
 // Need "Provider" for connect all components
@@ -8,28 +8,30 @@ const FeedbackContext = createContext();
 
 /* wrap all components as children in App.jsx */
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: "This is feedback item 1",
-      rating: 10,
-    },
-    {
-      id: 2,
-      text: "This is feedback item 2",
-      rating: 9,
-    },
-    {
-      id: 3,
-      text: "This is feedback item 3",
-      rating: 7,
-    },
-  ]);
-  const [feedbackEdit, setFeedbackEdit] = useState({ //create state object for add edited item
+  const [isLoading, setIsLoading] = useState(true) // 
+  // delete hard coded feedback and will use API db
+  const [feedback, setFeedback] = useState([]);
+
+  // render data from server when app start
+  useEffect(() => {
+    fetchfeedback()
+  }, []);
+
+  //fetch data from server
+  const fetchfeedback = async () => {
+    //sort by id using json server
+    const response = await fetch(`http://localhost:5000/feedback?_sort=id&_order=desc`);
+    const data = await response.json();
+
+    setFeedback(data)
+    setIsLoading(false)
+  };
+
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    //create state object for add edited item
     item: {},
     edit: false,
   });
-
   // set item to be updated
   const editFeedback = (item) => {
     setFeedbackEdit({
@@ -41,7 +43,9 @@ export const FeedbackProvider = ({ children }) => {
   // update feedbackitem (functionality of submit button in form when editing text)
   const updateFeedback = (idOfEdit, updItem) => {
     setFeedback(
-      feedback.map((item) => (item.id === idOfEdit ? { ...item, ...updItem } : item))
+      feedback.map((item) =>
+        item.id === idOfEdit ? { ...item, ...updItem } : item
+      )
     );
 
     // FIX: this fixes being able to add a feedback after editing
@@ -73,6 +77,7 @@ export const FeedbackProvider = ({ children }) => {
         editFeedback: editFeedback, // funnction run when click edit
         feedbackEdit: feedbackEdit, // variable of state that hold item boolean
         updateFeedback: updateFeedback, // function update item
+        isLoading: isLoading, //boolean loading data value
       }}
     >
       {children}
