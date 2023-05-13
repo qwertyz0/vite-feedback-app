3871cf7462ec3a643c203ff46b162189fca6dce5
YanisS <yanismac@MacBook-Pro-YanisS.local>
2023-05-13 16:47:11 +0300

    create proxy to server

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  //server objeck for proxy in VITE, coz default proxy in package json not work
+  server: {
+    proxy: {
+      "/feedback": "http://localhost:5000",
+    },
+  },
   plugins: [react()],
 })
