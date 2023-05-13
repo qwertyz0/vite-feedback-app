d1ea8fca6f7494d5f996b19ec42e68c6cc513788
YanisS <yanismac@MacBook-Pro-YanisS.local>
2023-05-13 16:48:53 +0300

    mdify add feedback function, to add feedbacks on server instead only in user ui

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -20,7 +20,7 @@ export const FeedbackProvider = ({ children }) => {
   //fetch data from server
   const fetchfeedback = async () => {
     //sort by id using json server
-    const response = await fetch(`http://localhost:5000/feedback?_sort=id&_order=desc`);
+    const response = await fetch(`/feedback?_sort=id&_order=desc`); //delete url, this is proxy in pakage.json
     const data = await response.json();
 
     setFeedback(data)
@@ -62,9 +62,21 @@ export const FeedbackProvider = ({ children }) => {
     }
   };
 
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4();
-    setFeedback([newFeedback, ...feedback]);
+  //Add feedback
+  const addFeedback = async (newFeedback) => {
+    //creating post response for adding data to server
+    const response = await fetch('/feedback', {
+      method: 'POST',
+      headers:{
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newFeedback), //add body that add new comments
+    })
+
+    const data = await response.json() //new data like old "newFeedback"
+
+    //newFeedback.id = uuidv4();
+    setFeedback([data, ...feedback]); // replace newFeedback on data
   };
 
   /*pass all states in value object "value={{...}}" */
